e37e237d5487bd7e5b00514a85029d5725fb89f8
fourtf <tf.four@gmail.com>
2022-04-28 11:27:40 +0200

    initial commit

---
+++ src/main/java/com/example/poker_hand/PokerHand.java
@@ -0,0 +1,9 @@
+package com.example.poker_hand;
+
+public class PokerHand implements Comparable<PokerHand> {
+
+    @Override
+    public int compareTo(PokerHand o) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/poker_hand/PokerHandTest.java
@@ -0,0 +1,11 @@
+package com.example.poker_hand;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class PokerHandTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
